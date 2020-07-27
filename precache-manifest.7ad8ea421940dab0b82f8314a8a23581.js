c5e89c871bfecaba52ab3cbc0ad84f6b79a04a02
Jonathan Ahn <jonathanahn95@gmail.com>
2020-07-26 20:11:41 -0400

    Updates

---
+++ precache-manifest.7ad8ea421940dab0b82f8314a8a23581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b81cb010afe3148601eb8d5303ddaf4",
+    "url": "/saga-take-home-test/index.html"
+  },
+  {
+    "revision": "3139bdf58bffbf9d432c",
+    "url": "/saga-take-home-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20d60d68209bb38153f2",
+    "url": "/saga-take-home-test/static/js/2.58bc8424.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saga-take-home-test/static/js/2.58bc8424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3139bdf58bffbf9d432c",
+    "url": "/saga-take-home-test/static/js/main.7193587d.chunk.js"
+  },
+  {
+    "revision": "6ad4075d59262939dea4",
+    "url": "/saga-take-home-test/static/js/runtime-main.c1523a03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/saga-take-home-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
