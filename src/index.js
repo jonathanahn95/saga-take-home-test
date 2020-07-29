368f0861f64d710eea34ac02a0caadc44d42fba9
Jonathan Ahn <jonathanahn95@gmail.com>
2020-07-29 16:19:17 -0400

    remove comments

--- src/index.js
+++ src/index.js
@@ -15,26 +15,20 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [
-  sagaMiddleware, // Redux-Saga
+  sagaMiddleware,
 ];
 
 const composeEnhancers =
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
     }) : compose;
 
 const enhancer = composeEnhancers(
   applyMiddleware(...middlewares),
-  // other store enhancers if any
 );
 
 const store = createStore(reducers, enhancer);
-
-// const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-
-
 
 sagaMiddleware.run(rootSaga);
 
